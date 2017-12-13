20e048a32f06e34e12da9b584aef684b8b835cc8
Aliisright <alihasan.me@me.com>
2017-12-13 23:57:01 +0100

    add table friends + controller de profil

---
+++ database/migrations/2017_12_13_134525_create_friends_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('friend_id')->unsigned()->index();
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('friend_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
